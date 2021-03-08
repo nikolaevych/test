a3559f885278a84bfb2c5f430b03548d4b03fcb8
nikolaevych <80172349+nikolaevych@users.noreply.github.com>
2021-03-08 11:30:46 +0200

    Update script.js

--- Browk-UI-KIT/Course catalog/js/script.js
+++ Browk-UI-KIT/Course catalog/js/script.js
@@ -1,5 +1,6 @@
 
 
+// шкала прогресса
 "use strict"
 const ratings = document.querySelectorAll('.scale_progress');
 
@@ -32,4 +33,53 @@ function setRatingActiveWidth(index = ratingValue.innerHTML) {
 		const ratingActiveWidth = index / 1;
 		ratingActive.style.width = `${ratingActiveWidth}%`;
 	}
-}
+};
+
+
+// меню бургер
+$(document).ready(function() {
+	$('.menu_burger').click(function(event) {
+		$('.menu_burger,.menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+}); 
+
+
+// слайдер
+$(document).ready(function(){
+	$('.slider').slick({
+		slidesToShow: 3,
+		arrows:false,
+		infinite:false,
+		centerMode:false,
+		variableWidth:true,
+		draggeble:false,
+		 responsive:[
+		 { 
+		 	breakpoint: 1360,
+	      settings:{
+				draggeble:true,
+				slidesToShow: 2
+	      }
+       },
+	    { 
+		 	breakpoint: 1024,
+	      settings: {
+		   	slidesToShow: 2,
+				arrows:false,
+				infinite:false,
+				centerMode:true,
+				variableWidth:true
+		}},
+		{
+			breakpoint: 900,
+	      settings:{
+	      	slidesToShow: 1,
+	      }
+
+		}]	
+	});
+});
+
+
+
